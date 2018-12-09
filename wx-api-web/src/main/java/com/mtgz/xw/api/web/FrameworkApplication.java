7c1e6bd2eef980f130e67858a5c3654043090db6
linyisheng <871692922@qq.com>
2018-12-09 23:08:18 +0800

    update

--- wx-api-web/src/main/java/com/mtgz/xw/api/web/FrameworkApplication.java
+++ wx-api-web/src/main/java/com/mtgz/xw/api/web/FrameworkApplication.java
@@ -1,13 +1,17 @@
 package com.mtgz.xw.api.web;
 
+import com.mtgz.xw.api.web.resolver.LoginUserHandlerMethodArgumentResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages = {"com.mtgz", "tech.fullink"})
 @Slf4j
@@ -15,9 +19,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDiscoveryClient
 @EnableScheduling
 @EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
-public class FrameworkApplication {
+public class FrameworkApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrameworkApplication.class, args);
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		super.addArgumentResolvers(argumentResolvers);
+		argumentResolvers.add(new LoginUserHandlerMethodArgumentResolver());
 	}
 }
